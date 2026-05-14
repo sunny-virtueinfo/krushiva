50fe18a3e854b4cb15470aee7208e19f2c16086a
sunny-virtueinfo <sunny19.virtueinfo@gmail.com>
2026-05-14 12:42:19 +0530

    feat: initialize Firebase and add AsyncStorage dependency to iOS project

--- ios/krushiva_RN/AppDelegate.swift
+++ ios/krushiva_RN/AppDelegate.swift
@@ -1,5 +1,6 @@
 import UIKit
 import React
+import FirebaseCore
 import React_RCTAppDelegate
 import ReactAppDependencyProvider
 
@@ -14,6 +15,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil
   ) -> Bool {
+    FirebaseApp.configure()
+    
     let delegate = ReactNativeDelegate()
     let factory = RCTReactNativeFactory(delegate: delegate)
     delegate.dependencyProvider = RCTAppDependencyProvider()
